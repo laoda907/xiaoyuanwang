b333b61bdd3b6100a239ae78c52e529cf4b74960
laoda907 <weiqi8712@gmail.com>
2026-02-01 17:17:57 +0800

    Create supabase-init.js

---
+++ supabase-init.js
@@ -0,0 +1,8 @@
+<script type="module">
+    import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+    window.supabase = createClient(
+        'https://akqrebzfodejodgrtzyc.supabase.co',
+        'sb_publishable_sClqqs5zvkGT8lTvQNxQOQ_p2e2j5to'
+    );
+</script>
